bc88f1608086b6866eea288d16556bb677157e41
Bruno Vittoria <brunovittoria1999@hotmail.com>
2023-06-09 14:49:35 -0300

    Updating banner, Links and Favicon

--- js-file/main.js
+++ js-file/main.js
@@ -4,3 +4,8 @@ document.querySelector(".card-btn").addEventListener("click", () => {
     document.querySelector(".container").classList.toggle("change");
   });
   
+// Funçao do botao pra CONTATO 
+
+function enviarEmail() {
+  window.open("mailto:brunogun1999@gmail.com", "_blank"); 
+}
